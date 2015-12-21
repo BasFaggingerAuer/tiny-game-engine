65a052a2e41f9cdf01c9d646e36684f97c47ce3f
takenu <takenu@gmail.com>
2015-12-21 22:54:48 +0100

    Also revert draw/animatedmeshhorde.h to original
    This one was missed previously, now there should really only be
    font-related differences in the pull request.

--- tiny/draw/animatedmeshhorde.h
+++ tiny/draw/animatedmeshhorde.h
@@ -89,10 +89,7 @@ class AnimatedMeshHorde : public Renderable
         }
         
         template <typename Iterator>
-        void setMeshes(Iterator first, Iterator last) { setInstances(first,last); }
-
-        template <typename Iterator>
-        void setInstances(Iterator first, Iterator last)
+        void setMeshes(Iterator first, Iterator last)
         {
             nrMeshes = 0;
             
