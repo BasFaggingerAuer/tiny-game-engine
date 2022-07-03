6ae35c9e66b79a42968a4da076f15ea18facadf0
BasFaggingerAuer <basfaggingerauer@gmail.com>
2022-07-03 15:58:31 +0200

    Working on more generic matrix/vector classes.

--- tiny/math/genmat.h
+++ tiny/math/genmat.h
@@ -16,6 +16,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 
+#include <type_traits>
+#include <array>
+#include <algorithm>
 #include <cassert>
 
 #include <tiny/math/vec.h>
@@ -29,13 +32,168 @@ template<typename t, size_t m, size_t n>
 class genmat
 {
     public:
-        genmat() = default;
-        genmat(const genmat &) = default;
-        ~genmat() = default;
-        
-        inline const t & operator () (const size_t i, const size_t j) const noexcept {assert(i < m && j < n); return v[m*j + i];};
-        inline t & operator () (const size_t i, const size_t j) noexcept {assert(i < m && j < n); return v[m*j + i];};
+        std::array<t, m*n> v;
+        typedef t value;
 
+    public:
+        genmat() = default;
+        genmat(const genmat<t, m, n> &) = default;
+        genmat(genmat<t, m, n> &&) = default;
+        genmat<t, m, n> & operator = (const genmat<t, m, n> &) = default;
+        genmat<t, m, n> & operator = (genmat<t, m, n> &&) = default;
+        ~genmat() = default;
+
+        //Specific constructors.
+        template <typename ...Args>
+        genmat(Args ...args) noexcept : v({args...}) {}
+
+        explicit genmat(const t &a) noexcept {v.fill(a);}
+        
+        //Coefficients.
+        inline const t & operator () (const size_t i, const size_t j) const noexcept {return v[m*j + i];};
+        inline t & operator () (const size_t i, const size_t j) noexcept {return v[m*j + i];};
+
+        //Coefficient-wise operations.
+        inline friend genmat<t, m, n> clamp(const genmat<t, m, n> &a, const genmat<t, m, n> &b, const genmat<t, m, n> &c) noexcept
+        {
+            genmat<t, m, n> d;
+            for (size_t i = 0; i < m*n; ++i) d.v[i] = clamp(a.v[i], b.v[i], c.v[i]);
+            return d;
+        }
+        inline friend genmat<t, m, n> step(const genmat<t, m, n> &a, const genmat<t, m, n> &b) noexcept
+        {
+            genmat<t, m, n> c;
+            for (size_t i = 0; i < m*n; ++i) c.v[i] = step(a.v[i], b.v[i]);
+            return c;
+        }
+        inline friend genmat<t, m, n> min(const genmat<t, m, n> &a, const genmat<t, m, n> &b) noexcept
+        {
+            genmat<t, m, n> c;
+            for (size_t i = 0; i < m*n; ++i) c.v[i] = std::min<t>(a.v[i], b.v[i]);
+            return c;
+        }
+        inline friend genmat<t, m, n> max(const genmat<t, m, n> &a, const genmat<t, m, n> &b) noexcept
+        {
+            genmat<t, m, n> c;
+            for (size_t i = 0; i < m*n; ++i) c.v[i] = std::max<t>(a.v[i], b.v[i]);
+            return c;
+        }
+        inline friend genmat<t, m, n> abs(const genmat<t, m, n> &a) noexcept
+        {
+            genmat<t, m, n> b;
+            for (size_t i = 0; i < m*n; ++i) b.v[i] = t_abs(a.v[i]);
+            return b;
+        }
+        inline friend genmat<t, m, n> floor(const genmat<t, m, n> &a) noexcept
+        {
+            genmat<t, m, n> b;
+            for (size_t i = 0; i < m*n; ++i) b.v[i] = std::floor(a.v[i]);
+            return b;
+        }
+        inline friend genmat<t, m, n> ceil(const genmat<t, m, n> &a) noexcept
+        {
+            genmat<t, m, n> b;
+            for (size_t i = 0; i < m*n; ++i) b.v[i] = std::ceil(a.v[i]);
+            return b;
+        }
+        inline friend genmat<t, m, n> fract(const genmat<t, m, n> &a) noexcept
+        {
+            genmat<t, m, n> b;
+            for (size_t i = 0; i < m*n; ++i) b.v[i] = fract(a.v[i]);
+            return b;
+        }
+
+        //Equality.
+        inline bool operator == (const genmat<t, m, n> &a) const noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) if (v[i] != a.v[i]) return false;
+            return true;
+        }
+
+        inline bool operator != (const genmat<t, m, n> &a) const noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) if (v[i] != a.v[i]) return true;
+            return false;
+        }
+        
+        //Addition.
+        inline genmat<t, m, n> & operator += (const genmat<t, m, n> &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] += a.v[i];
+            return *this;
+        }
+
+        inline genmat<t, m, n> & operator += (genmat<t, m, n> &&a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] += a.v[i];
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator + (genmat<t, m, n> a, const genmat<t, m, n> &b) noexcept {return a += b;}
+        
+        inline genmat<t, m, n> & operator += (const t &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] += a;
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator + (genmat<t, m, n> a, const t &b) noexcept {return a += b;}
+
+        //Subtraction.
+        inline genmat<t, m, n> & operator -= (const genmat<t, m, n> &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] -= a.v[i];
+            return *this;
+        }
+
+        inline genmat<t, m, n> & operator -= (genmat<t, m, n> &&a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] -= a.v[i];
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator - (genmat<t, m, n> a, const genmat<t, m, n> &b) noexcept {return a -= b;}
+        
+        inline genmat<t, m, n> & operator -= (const t &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] -= a;
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator - (genmat<t, m, n> a, const t &b) noexcept {return a -= b;}
+
+        inline genmat<t, m, n> operator - () const noexcept
+        {
+            genmat<t, m, n> a;
+            for (size_t i = 0; i < m*n; ++i) a.v[i] = -v[i];
+            return a;
+        }
+
+        //Scalar multiplication.
+        inline genmat<t, m, n> & operator *= (const t &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] *= a;
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator * (genmat<t, m, n> a, const t &b) noexcept {return a *= b;}
+        inline friend genmat<t, m, n> operator * (const t &b, genmat<t, m, n> a) noexcept {return a *= b;}
+        
+        inline genmat<t, m, n> & operator /= (const t &a) noexcept
+        {
+            for (size_t i = 0; i < m*n; ++i) v[i] /= a;
+            return *this;
+        }
+
+        inline friend genmat<t, m, n> operator / (genmat<t, m, n> a, const t &b) noexcept {return a /= b;}
+        inline friend genmat<t, m, n> operator / (const t &b, genmat<t, m, n> a) noexcept
+        {
+            genmat<t, m, n> c;
+            for (size_t i = 0; i < m*n; ++i) c.v[i] = b/a.v[i];
+            return c;
+        }
+
+        //Matrix multiplication.
         template<size_t k>
         genmat<t, m, k> operator * (const genmat<t, n, k> &a) const noexcept
         {
@@ -45,7 +203,7 @@ class genmat
             {
                 for (size_t j = 0; j < k; ++j)
                 {
-                    t s = t(0.0);
+                    t s = t(0);
 
                     for (size_t l = 0; l < n; ++l)
                     {
@@ -58,7 +216,8 @@ class genmat
             
             return b;
         }
-
+        
+        //Transposition.
         genmat<t, n, m> transpose() const noexcept
         {
             genmat<t, n, m> a;
@@ -74,54 +233,6 @@ class genmat
             return a;
         }
 
-        genmat<t, m, n> operator + (const genmat<t, m, n> &a) const noexcept
-        {
-            genmat<t, m, n> b;
-
-            for (size_t i = 0; i < m*n; ++i)
-            {
-                b.v[i] = v[i] + a.v[i];
-            }
-
-            return b;
-        }
-
-        genmat<t, m, n> operator - (const genmat<t, m, n> &a) const noexcept
-        {
-            genmat<t, m, n> b;
-
-            for (size_t i = 0; i < m*n; ++i)
-            {
-                b.v[i] = v[i] - a.v[i];
-            }
-
-            return b;
-        }
-
-        genmat<t, m, n> operator * (const t &a) const noexcept
-        {
-            genmat<t, m, n> b;
-
-            for (size_t i = 0; i < m*n; ++i)
-            {
-                b.v[i] = v[i]*a;
-            }
-
-            return b;
-        }
-
-        genmat<t, m, n> operator / (const t &a) const noexcept
-        {
-            genmat<t, m, n> b;
-
-            for (size_t i = 0; i < m*n; ++i)
-            {
-                b.v[i] = v[i]/a;
-            }
-
-            return b;
-        }
-
         genmat<t, (m <= n ? m : n), 1> getDiagonal() const noexcept
         {
             genmat<t, (m <= n ? m : n), 1> a;
@@ -133,24 +244,21 @@ class genmat
 
             return a;
         }
-
-        t getFrobeniusNorm() const noexcept
+        
+        //Frobenius norm.
+        friend t norm(const genmat<t, m, n> &a) noexcept
         {
-            t f = 0.0;
-
-            for (size_t i = 0; i < m*n; ++i)
-            {
-                f += v[i]*v[i];
-            }
-
+            t f = t(0);
+            for (size_t i = 0; i < m*n; ++i) f += a.v[i]*a.v[i];
             return std::sqrt(f);
         }
-
+        
+        //Create specific matrices.
         static genmat<t, m, n> Zero() noexcept
         {
             genmat<t, m, n> a;
 
-            a.v.fill(t(0.0));
+            a.v.fill(t(0));
 
             return a;
         }
@@ -159,11 +267,11 @@ class genmat
         {
             genmat<t, m, n> a;
 
-            a.v.fill(t(0.0));
+            a.v.fill(t(0));
 
             for (size_t i = 0; i < std::min(m, n); ++i)
             {
-                a(i, i) = t(1.0);
+                a(i, i) = t(1);
             }
 
             return a;
@@ -189,41 +297,97 @@ class genmat
 
             return Out;
         }
-
-        std::array<t, m*n> v;
 };
 
-template<typename t, size_t m, size_t n>
-genmat<t, m, n> operator * (const t &a, const genmat<t, m, n> &b) noexcept
+//Generic column vector.
+template<typename t, size_t m>
+class genvec : public genmat<t, m, 1>
 {
-    genmat<t, m, n> c;
+    public:
+        genvec() = default;
+        genvec(const genvec<t, m> &) = default;
+        genvec(genvec<t, m> &&) = default;
+        genvec<t, m> & operator = (const genvec<t, m> &) = default;
+        genvec<t, m> & operator = (genvec<t, m> &&) = default;
+        ~genvec() = default;
+        
+        //Specific constructors.
+        template <typename ...Args>
+        genvec(Args ...args) noexcept : genmat<t, m, 1>(args...) {}
 
-    for (size_t i = 0; i < m*n; ++i)
-    {
-        c.v[i] = a*b.v[i];
-    }
+        explicit genvec(const t &a) noexcept {this->v.fill(a);}
 
-    return b;
-}
+        //Coefficients.
+        inline const t & operator () (const size_t i) const noexcept {return this->v[i];};
+        inline t & operator () (const size_t i) noexcept {return this->v[i];};
 
-template<typename t, size_t m, size_t n>
-genmat<t, m, n> operator / (const t &a, const genmat<t, m, n> &b) noexcept
-{
-    genmat<t, m, n> c;
+        //Outer product.
+        template <size_t n = m>
+        typename std::enable_if_t<n == 3, genvec<t, m>>
+        inline friend cross(const genvec<t, m> &a, const genvec<t, m> &b) noexcept
+        {
+            return genvec<t, m>(a.v[1]*b.v[2] - a.v[2]*b.v[1],
+                                a.v[2]*b.v[0] - a.v[0]*b.v[2],
+                                a.v[0]*b.v[1] - a.v[1]*b.v[0]);
+        }
 
-    for (size_t i = 0; i < m*n; ++i)
-    {
-        c.v[i] = a/b.v[i];
-    }
+        //Inner products and lengths.
+        inline friend t dot(const genvec<t, m> &a, const genvec<t, m> &b) noexcept
+        {
+            t d = t(0);
+            for (size_t i = 0; i < m; ++i) d += a.v[i]*b.v[i];
+            return d;
+        }
+        
+        inline friend t length(const genvec<t, m> &a) noexcept {return std::sqrt(dot(a, a));}
+        inline friend t length2(const genvec<t, m> &a) noexcept {return dot(a, a);}
+        inline friend genvec<t, m> normalize(const genvec<t, m> &a) noexcept {return a/std::max<t>(length(a), EPS);}
+        
+        inline friend t minComponent(const genvec<t, m> &a) noexcept {return *std::min_element(a.v.begin(), a.v.end());}
+        inline friend t maxComponent(const genvec<t, m> &a) noexcept {return *std::max_element(a.v.begin(), a.v.end());}
+    
+        //Hadamard element-wise product for vectors.
+        //FIXME: This is dangerous since we use the same operator for matrix-multiplication!
+        inline genvec<t, m> & operator *= (const genvec<t, m> &a) noexcept
+        {
+            for (size_t i = 0; i < m; ++i) this->v[i] *= a.v[i];
+            return *this;
+        }
 
-    return b;
-}
+        inline genvec<t, m> & operator *= (genvec<t, m> &&a) noexcept
+        {
+            for (size_t i = 0; i < m; ++i) this->v[i] *= a.v[i];
+            return *this;
+        }
+
+        inline friend genvec<t, m> operator * (genvec<t, m> a, const genvec<t, m> &b) noexcept {return a += b;}
+
+        //Access coefficients and swizzle vectors.
+        inline t x() const noexcept {return this->v[0];}
+        inline t y() const noexcept {if constexpr (m >= 2) {return this->v[1];} else {return t(0);}}
+        inline t z() const noexcept {if constexpr (m >= 3) {return this->v[2];} else {return t(0);}}
+        inline t w() const noexcept {if constexpr (m >= 4) {return this->v[3];} else {return t(0);}}
+
+        inline genvec<t, 2> xy() const noexcept {if constexpr (m >= 2) {return genvec<t, 2>(this->v[0], this->v[1]);} else {return genvec<t, 2>(t(0));}}
+        inline genvec<t, 2> yx() const noexcept {if constexpr (m >= 2) {return genvec<t, 2>(this->v[1], this->v[0]);} else {return genvec<t, 2>(t(0));}}
+        
+        inline genvec<t, 3> xyz() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[0], this->v[1], this->v[2]);} else {return genvec<t, 3>(t(0));}}
+        inline genvec<t, 3> xzy() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[0], this->v[2], this->v[1]);} else {return genvec<t, 3>(t(0));}}
+        inline genvec<t, 3> zxy() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[2], this->v[0], this->v[1]);} else {return genvec<t, 3>(t(0));}}
+        inline genvec<t, 3> zyx() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[2], this->v[1], this->v[0]);} else {return genvec<t, 3>(t(0));}}
+        inline genvec<t, 3> yzx() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[1], this->v[2], this->v[0]);} else {return genvec<t, 3>(t(0));}}
+        inline genvec<t, 3> yxz() const noexcept {if constexpr (m >= 3) {return genvec<t, 3>(this->v[1], this->v[0], this->v[2]);} else {return genvec<t, 3>(t(0));}}
+};
+
+//Force instantiation.
+template class genmat<float, 3, 3>;
+template class genvec<float, 3>;
 
 genmat<float, 3, 3> fromFixedSizeMatrix(const mat3 &a) noexcept
 {
-    return genmat<float, 3, 3>({{a.v00, a.v10, a.v20,
-                                 a.v01, a.v11, a.v21,
-                                 a.v02, a.v12, a.v22}});
+    return genmat<float, 3, 3>(a.v00, a.v10, a.v20,
+                               a.v01, a.v11, a.v21,
+                               a.v02, a.v12, a.v22);
 }
 
 mat3 toFixedSizeMatrix(const genmat<float, 3, 3> &a) noexcept
@@ -312,7 +476,7 @@ std::tuple<genmat<t, n, 1>, genmat<t, n, n>> eigenDecompositionSym(genmat<t, n, 
     }
 
     //Check that we found the eigenvectors.
-    assert((aCheck - e*a*e.transpose()).getFrobeniusNorm() < EPS);
+    assert(norm(aCheck - e*a*e.transpose()) < EPS);
 #endif
     
     //Eigenvalues are on the diagonal.
